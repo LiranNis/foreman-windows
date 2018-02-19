6c687dd0cc9bd29489eac62f8daa8b78b75cb843
LiranNis <lirannis@gmail.com>
2018-02-19 19:28:59 +0200

    Added the snippet to inject_hammer, fixed secondary dns server, added static configuration of bonds and interfaces

--- templates/inject_hammer.sh
+++ templates/inject_hammer.sh
@@ -8,12 +8,14 @@ base_command="hammer --verify-ssl false -u $user -p $pass"
 
 # 1. Add new architecture x64
 $base_command architecture create --name x64
+
 # 2. Add new operating system Name:Windows, Major:6, Minor:3, Family:Windows, RootPassHash:Base64, Architectures:x64
 $base_command os create --architectures x64 --family Windows --name Windows --description "Windows Server 2016" --password-hash Base64 --major 10 --minor 0
 $base_command os create --architectures x64,i386 --family Windows --name Windows --description "Windows Server 2012 R2" --password-hash Base64 --major 6 --minor 3
 $base_command os create --architectures x64,i386 --family Windows --name Windows --description "Windows Server 2012" --password-hash Base64 --major 6 --minor 2
 $base_command os create --architectures x64,i386 --family Windows --name Windows --description "Windows Server 2008 R2" --password-hash Base64 --major 6 --minor 1
 $base_command os create --architectures x64,i386 --family Windows --name Windows --description "Windows Server 2008" --password-hash Base64 --major 6 --minor 0
+
 # 3. Add templates
 operatingsystems='"Windows Server 2008 R2","Windows Server 2012 R2","Windows Server 2016"'
 $base_command template create --operatingsystems "Windows Server 2008","Windows Server 2008 R2","Windows Server 2012","Windows Server 2012 R2","Windows Server 2016" --file wimaging_finish.erb --name "Wimaging finish" --type finish
@@ -24,6 +26,8 @@ $base_command template create --operatingsystems "Windows Server 2008","Windows 
 $base_command template create --operatingsystems "Windows Server 2008","Windows Server 2008 R2","Windows Server 2012","Windows Server 2012 R2","Windows Server 2016" --file wimaging_snip_local_users.xml.erb --name "Wimaging local users" --type "snippet"
 $base_command template create --operatingsystems "Windows Server 2008","Windows Server 2008 R2","Windows Server 2012","Windows Server 2012 R2","Windows Server 2016" --file wimaging_snip_ou_from_hostgroup.erb --name "Wimaging OU from Hostgroup" --type "snippet"
 $base_command template create --operatingsystems "Windows Server 2008","Windows Server 2008 R2","Windows Server 2012","Windows Server 2012 R2","Windows Server 2016" --file wimaging_userdata_joinDomain.ps1.erb --name "Wimaging joinDomain.ps1" --type "user_data"
+$base_command template create --operatingsystems "Windows Server 2008","Windows Server 2008 R2","Windows Server 2012","Windows Server 2012 R2","Windows Server 2016" --file snip_windows_networking_setup.erb --name "windows_networking_setup" --type "snippet"
+
 # 4. Add partition table and associate it
 $base_command partition-table create --os-family Windows --name "Windows default" --file "wimaging_partition_table.erb"  --operatingsystems "Windows Server 2008","Windows Server 2008 R2","Windows Server 2012","Windows Server 2012 R2","Windows Server 2016"
 
